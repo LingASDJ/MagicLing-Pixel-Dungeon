b1ced704971457f257ebe8784b4f7253ca267e33
Evan Debenham <Evan@ShatteredPixel.com>
2024-06-24 12:34:11 -0400

    v2.5.0: converted notes tab to use new grid UI

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/journal/Notes.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/journal/Notes.java
@@ -21,9 +21,24 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.journal;
 
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.items.keys.Key;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.plants.Sungrass;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.BlacksmithSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.GhostSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ImpSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ShopkeeperSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.SpawnerSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.StatueSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.WandmakerSprite;
+import com.shatteredpixel.shatteredpixeldungeon.tiles.TerrainFeaturesTilemap;
+import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIcon;
+import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
+import com.shatteredpixel.shatteredpixeldungeon.ui.Icons;
+import com.watabou.noosa.Image;
 import com.watabou.utils.Bundlable;
 import com.watabou.utils.Bundle;
 
@@ -31,7 +46,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Notes {
-
+	
 	public static abstract class Record implements Comparable<Record>, Bundlable {
 
 		//TODO currently notes can only relate to branch = 0, add branch support here if that changes
@@ -41,18 +56,24 @@ public class Notes {
 			return depth;
 		}
 
-		public abstract String desc();
+		public Image icon() { return Icons.STAIRS.get(); }
 
+		public int quantity() { return 1; }
+		
+		public abstract String title();
+
+		public abstract String desc();
+		
 		@Override
 		public abstract boolean equals(Object obj);
-
+		
 		@Override
 		public int compareTo( Record another ) {
 			return another.depth() - depth();
 		}
-
+		
 		private static final String DEPTH	= "depth";
-
+		
 		@Override
 		public void restoreFromBundle( Bundle bundle ) {
 			depth = bundle.getInt( DEPTH );
@@ -63,49 +84,81 @@ public class Notes {
 			bundle.put( DEPTH, depth );
 		}
 	}
-
+	
 	public enum Landmark {
 		WELL_OF_HEALTH,
 		WELL_OF_AWARENESS,
-		WELL_OF_TRANSMUTATION,
-
-		WELL_OF_OIL,
-
 		ALCHEMY,
 		GARDEN,
 		STATUE,
 		SACRIFICIAL_FIRE,
 		SHOP,
-
+		
 		GHOST,
 		WANDMAKER,
 		TROLL,
 		IMP,
-		SHOP2,
-		REDAGRON,
-		DEMON_SPAWNER,
 
-		SMALLB;
+		DEMON_SPAWNER;
 
-		public String desc() {
+		public String title() {
 			return Messages.get(this, name());
 		}
 	}
-
+	
 	public static class LandmarkRecord extends Record {
-
+		
 		protected Landmark landmark;
-
+		
 		public LandmarkRecord() {}
-
+		
 		public LandmarkRecord(Landmark landmark, int depth ) {
 			this.landmark = landmark;
 			this.depth = depth;
 		}
 
+		public Image icon(){
+			switch (landmark){
+				default:
+					return super.icon();
+
+				//TODO we probably want a separate image file for landmark visuals, especially if we expand this
+				case WELL_OF_HEALTH:
+				case WELL_OF_AWARENESS:
+					return new Image(Assets.Environment.TILES_SEWERS, 48, 16, 16, 16);
+				case ALCHEMY:
+					return new Image(Assets.Environment.TILES_SEWERS, 0, 64, 16, 16);
+				case GARDEN:
+					return TerrainFeaturesTilemap.getPlantVisual(new Sungrass());
+				case STATUE:
+					return new Image(new StatueSprite());
+				case SACRIFICIAL_FIRE:
+					return new BuffIcon(BuffIndicator.SACRIFICE, true);
+				case SHOP:
+					return new Image(new ShopkeeperSprite());
+
+				case GHOST:
+					return new Image(new GhostSprite());
+				case WANDMAKER:
+					return new Image(new WandmakerSprite());
+				case TROLL:
+					return new Image(new BlacksmithSprite());
+				case IMP:
+					return new Image(new ImpSprite());
+
+				case DEMON_SPAWNER:
+					return new Image(new SpawnerSprite());
+			}
+		}
+
+		@Override
+		public String title() {
+			return landmark.title();
+		}
+
 		@Override
 		public String desc() {
-			return landmark.desc();
+			return "";
 		}
 
 		@Override
@@ -114,42 +167,52 @@ public class Notes {
 					&& landmark == ((LandmarkRecord) obj).landmark
 					&& depth() == ((LandmarkRecord) obj).depth();
 		}
-
+		
 		private static final String LANDMARK	= "landmark";
-
+		
 		@Override
 		public void restoreFromBundle(Bundle bundle) {
 			super.restoreFromBundle(bundle);
 			landmark = Landmark.valueOf(bundle.getString(LANDMARK));
 		}
-
+		
 		@Override
 		public void storeInBundle(Bundle bundle) {
 			super.storeInBundle(bundle);
 			bundle.put( LANDMARK, landmark.name() );
 		}
 	}
-
+	
 	public static class KeyRecord extends Record {
-
+		
 		protected Key key;
-
+		
 		public KeyRecord() {}
-
+		
 		public KeyRecord( Key key ){
 			this.key = key;
 		}
-
+		
 		@Override
 		public int depth() {
 			return key.depth;
 		}
 
 		@Override
-		public String desc() {
+		public Image icon() {
+			return new ItemSprite(key);
+		}
+
+		@Override
+		public String title() {
 			return key.title();
 		}
 
+		@Override
+		public String desc() {
+			return key.desc();
+		}
+		
 		public Class<? extends Key> type(){
 			return key.getClass();
 		}
@@ -157,51 +220,51 @@ public class Notes {
 		public int quantity(){
 			return key.quantity();
 		}
-
+		
 		public void quantity(int num){
 			key.quantity(num);
 		}
-
+		
 		@Override
 		public boolean equals(Object obj) {
 			return (obj instanceof KeyRecord)
 					&& key.isSimilar(((KeyRecord) obj).key);
 		}
-
+		
 		private static final String KEY	= "key";
-
+		
 		@Override
 		public void restoreFromBundle(Bundle bundle) {
 			super.restoreFromBundle(bundle);
 			key = (Key) bundle.get(KEY);
 		}
-
+		
 		@Override
 		public void storeInBundle(Bundle bundle) {
 			super.storeInBundle(bundle);
 			bundle.put( KEY, key );
 		}
 	}
-
+	
 	private static ArrayList<Record> records;
-
+	
 	public static void reset() {
 		records = new ArrayList<>();
 	}
-
+	
 	private static final String RECORDS	= "records";
-
+	
 	public static void storeInBundle( Bundle bundle ) {
 		bundle.put( RECORDS, records );
 	}
-
+	
 	public static void restoreFromBundle( Bundle bundle ) {
 		records = new ArrayList<>();
 		for (Bundlable rec : bundle.getCollection( RECORDS ) ) {
 			records.add( (Record) rec );
 		}
 	}
-
+	
 	public static boolean add( Landmark landmark ) {
 		LandmarkRecord l = new LandmarkRecord( landmark, Dungeon.depth );
 		if (!records.contains(l)) {
@@ -211,11 +274,11 @@ public class Notes {
 		}
 		return false;
 	}
-
+	
 	public static boolean remove( Landmark landmark ) {
 		return records.remove( new LandmarkRecord(landmark, Dungeon.depth) );
 	}
-
+	
 	public static boolean add( Key key ){
 		KeyRecord k = new KeyRecord(key);
 		if (!records.contains(k)){
@@ -228,7 +291,7 @@ public class Notes {
 			return true;
 		}
 	}
-
+	
 	public static boolean remove( Key key ){
 		KeyRecord k = new KeyRecord( key );
 		if (records.contains(k)){
@@ -242,7 +305,7 @@ public class Notes {
 		}
 		return false;
 	}
-
+	
 	public static int keyCount( Key key ){
 		KeyRecord k = new KeyRecord( key );
 		if (records.contains(k)){
@@ -252,11 +315,11 @@ public class Notes {
 			return 0;
 		}
 	}
-
+	
 	public static ArrayList<Record> getRecords(){
 		return getRecords(Record.class);
 	}
-
+	
 	public static <T extends Record> ArrayList<T> getRecords( Class<T> recordType ){
 		ArrayList<T> filtered = new ArrayList<>();
 		for (Record rec : records){
@@ -267,8 +330,23 @@ public class Notes {
 		return filtered;
 	}
 
+	public static ArrayList<Record> getRecords(int depth){
+		ArrayList<Record> filtered = new ArrayList<>();
+		for (Record rec : records){
+			if (rec.depth() == depth){
+				if (rec instanceof KeyRecord){
+					filtered.add(rec); //key records always go at the end
+				} else {
+					filtered.add(0, rec);
+				}
+
+			}
+		}
+		return filtered;
+	}
+	
 	public static void remove( Record rec ){
 		records.remove(rec);
 	}
-
+	
 }
