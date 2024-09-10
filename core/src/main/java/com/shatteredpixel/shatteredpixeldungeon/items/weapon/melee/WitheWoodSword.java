5bae458444589f3bd1df82a990184c26fb063d9b
flash <1602798557@qq.com>
2024-09-10 09:23:06 +0800

    1.狂战士文本修复 2.角斗士连击上限 3.排山倒海修正降低 4.部分武器的精准/攻速异常修复 5.毒杖修改

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WitheWoodSword.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WitheWoodSword.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2012-2015 Oleg Dolya
  *
  * Shattered Pixel Dungeon
- * Copyright (C) 2014-2024 Evan Debenham
+ * Copyright (C) 2014-2021 Evan Debenham
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,11 +22,18 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Burning;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Terror;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Vertigo;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.utils.Random;
 
-public class WarHammer extends MeleeWeapon {
+public class WitheWoodSword extends MeleeWeapon {
 
 	{
 		image = ItemSpriteSheet.WAR_HAMMER;
@@ -34,7 +41,22 @@ public class WarHammer extends MeleeWeapon {
 		hitSoundPitch = 1f;
 
 		tier = 5;
-		ACC = 1.20f; //20% boost to accuracy
+		ACC = 1f; //90% boost to accuracy
+		DLY = 1.50f; //1.5x speed
+	}
+
+	@Override
+	public String targetingPrompt() {
+		return Messages.get(this, "prompt");
+	}
+
+	@Override
+	protected int baseChargeUse(Hero hero, Char target){
+		if (target == null || (target instanceof Mob && ((Mob) target).surprisedBy(hero))) {
+			return 1;
+		} else {
+			return 2;
+		}
 	}
 
 	@Override
@@ -44,8 +66,19 @@ public class WarHammer extends MeleeWeapon {
 	}
 
 	@Override
-	public String targetingPrompt() {
-		return Messages.get(this, "prompt");
+	public int proc(Char attacker, Char defender, int damage ) {
+		switch (Random.Int(6)) {
+			case 0:case 1:case 2:case 3:
+			default:
+				return super.proc( attacker, defender, damage );
+			case 4:case 5:
+				Buff.prolong(defender, Vertigo.class, 12f+level);
+				Buff.prolong(defender, Terror.class, 12f+level);
+				Buff.affect(defender, Burning.class).reignite(defender,5f+level);
+				//Over Level 3,Not Debuff
+				return super.proc( attacker, defender, damage );
+		}
+
 	}
 
 	@Override
@@ -53,21 +86,6 @@ public class WarHammer extends MeleeWeapon {
 		//+(6+1.5*lvl) damage, roughly +40% base dmg, +45% scaling
 		int dmgBoost = augment.damageFactor(6 + Math.round(1.5f*buffedLvl()));
 		Mace.heavyBlowAbility(hero, target, 1, dmgBoost, this);
-	}
-
-	public String upgradeAbilityStat(int level){
-		int dmgBoost = 6 + Math.round(1.5f*level);
-		return augment.damageFactor(min(level)+dmgBoost) + "-" + augment.damageFactor(max(level)+dmgBoost);
-	}
-
-	@Override
-	public String abilityInfo() {
-		int dmgBoost = levelKnown ? 6 + Math.round(1.5f*buffedLvl()) : 6;
-		if (levelKnown){
-			return Messages.get(this, "ability_desc", augment.damageFactor(min()+dmgBoost), augment.damageFactor(max()+dmgBoost));
-		} else {
-			return Messages.get(this, "typical_ability_desc", min(0)+dmgBoost, max(0)+dmgBoost);
-		}
 	}
 
 }
