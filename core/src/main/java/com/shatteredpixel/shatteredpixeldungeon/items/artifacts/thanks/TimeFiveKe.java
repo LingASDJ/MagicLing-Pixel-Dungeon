e5a77133787ae8423ce68d804fd3a2197cb0aeff
Prana Ling <70191651+LingASDJ@users.noreply.github.com>
2024-12-02 20:23:21 +0800

    Delete TimeFiveKe.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/thanks/TimeFiveKe.java
+++
@@ -1,7 +0,0 @@
-package com.shatteredpixel.shatteredpixeldungeon.items.artifacts.thanks;
-
-import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.Artifact;
-
-public class TimeFiveKe extends Artifact {
-
-}
